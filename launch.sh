ae6ee8bc341bc02c2f4df366625d2def7e9fdbe2
Jack Baron <jackmbaron@gmail.com>
2020-05-20 08:01:13 +0100

    ensure ops file isn't overwritten on restarts

--- launch.sh
+++ launch.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
-echo $SERVER_OP > /minecraft/ops.txt
+
+# Force Server Operator
+if ! { [ -z ${SERVER_OP+x} ] || [ -z "$SERVER_OP" ]; }
+then
+  echo "$SERVER_OP"
+  grep -sqxF "$SERVER_OP" ops.txt || echo "$SERVER_OP" >> ops.txt
+fi
+
+# Run Server
 java $JAVA_ARGS -jar Tekkit.jar nogui
